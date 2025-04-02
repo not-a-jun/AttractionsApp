82ae8c05e83e184225a11710eea70d9ebc388339
IvanNikolaev1 <smoking.zd@yandex.ru>
2025-04-03 02:02:42 +0300

    Version 1

--- src/main/java/com/AstonProgect/exception/ResourceNotFoundException.java
+++ src/main/java/com/AstonProgect/exception/ResourceNotFoundException.java
@@ -7,8 +7,6 @@ package com.AstonProgect.exception;
  * что запрошенная сущность (например, Attraction, Address, TicketInfo и т.д.)
  * не существует в базе данных. Оно обрабатывается {@link GlobalExceptionHandler},
  * который преобразует его в HTTP-ответ со статусом 404 Not Found.
- * 
- * @see GlobalExceptionHandler#handleResourceNotFoundException(ResourceNotFoundException, org.springframework.web.context.request.WebRequest)
  */
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
